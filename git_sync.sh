8d949910aa2b1b1add46996e538d6297e6dd4469
Tommy <tom.liang.au@gmail.com>
2017-02-10 00:39:21 +1000

    detect index/remote change before doing pull/push

--- git_sync.sh
+++ git_sync.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-~/.bashrc	#run ssh agent script
+~/.bashrc
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 curr_folder=${PWD}
 echo 'solution folder:'$curr_folder
 
 for f in $( ls ); do
-	echo $f
 	if [ -d $f ]; then
+		echo '=======['$f']======='
 		cd $curr_folder/$f
-		git pull origin master 
+		if git diff-index --quiet HEAD --; then
+		  # no index changes, test remote diff
+		  if [ -n "$(git diff origin/master..HEAD)" ]; then
+		  	echo '---pulling---'
+				git pull origin master 
+		  	echo '---pushing---'
+				git push origin master
+			fi
+		else
+			echo -e $RED$f':Changes need to commit first'$NC
+		fi
 
 		cd $curr_folder
 	fi
